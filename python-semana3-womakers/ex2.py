131c2e886e6f7ff2c7d4f1cf54659e63f9ade6ef
maisarp <maisarpiress@gmail.com>
2024-01-20 03:12:34 -0300

    exercicio 2

---
+++ python-semana3-womakers/ex2.py
@@ -0,0 +1,6 @@
+'''Peça ao usuário para informar o ano de nascimento. Em seguida,
+calcule e imprima a idade atual.'''
+
+nascimento = int(input ('Insira o ano de seu nascimento:'))
+idade = 2024-(nascimento)
+print(f'Sua idade atual é {idade} anos')
