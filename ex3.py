34279b8c92e333b4f4478da848623baa411589c2
maisarp <maisarpiress@gmail.com>
2024-01-20 03:31:05 -0300

    ex4

---
+++ ex3.py
@@ -0,0 +1,8 @@
+'''Faça um Programa que peça a quantidade de quilômetros, transforme
+em metros, centímetros e milímetros.'''
+
+km = int (input('Informe a quantidade de quilômetros: '))
+metros = km*1000
+cm = metros*100
+mm = cm*10
+print(f'{km} km correspondem a {metros} metros, {cm} centímetros e {mm} milímetros.')
