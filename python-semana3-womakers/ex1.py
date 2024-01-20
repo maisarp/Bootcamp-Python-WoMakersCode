acc40b5779c9e75d1bedc4f9375d4030c47389d9
maisarp <maisarpiress@gmail.com>
2024-01-20 02:54:41 -0300

    teste envio do ex1

---
+++ python-semana3-womakers/ex1.py
@@ -0,0 +1,25 @@
+'''Faça um Programa que peça dois números, realize as principais operações soma, subtração, multiplicação, divisão'''
+
+#soma 
+'''numero1 = int(input ('Digite o numero 01:'))
+numero2 = int(input ('Digite o numero 02:'))
+soma = numero1+numero2
+print(f'A soma dos números digitados é {soma}')'''
+
+#subtracao
+'''numero1 = int(input ('Digite o numero 01:'))
+numero2 = int(input ('Digite o numero 02:'))
+subtracao = numero1-numero2
+print(f'A subtração dos números digitados é {subtracao}')'''
+
+#divisao
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+divisao = numero1/numero2
+print(f'A divisão dos números digitados é: {divisao}')'''
+
+#multiplicacao
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+multiplicacao = numero1*numero2
+print(f'A multiplicação dos numeros digitados é {multiplicacao}')'''
