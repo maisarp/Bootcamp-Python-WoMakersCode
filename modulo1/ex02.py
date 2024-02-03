92c2f52d7c7ae73abe9cdaee97dc81d5de57b7a3
Maisa Pires <127149648+maisarp@users.noreply.github.com>
2024-02-03 14:22:00 -0300

    Delete modulo1 directory

--- modulo1/ex02.py
+++
@@ -1,9 +0,0 @@
-'''Peça ao usuário para informar o ano de nascimento. Em seguida,
-calcule e imprima a idade atual.'''
-
-nascimento = int(input ('Insira o ano de seu nascimento:'))
-
-#Cálculo da idade
-idade = 2024-(nascimento)
-
-print(f'Sua idade atual é {idade} anos')
